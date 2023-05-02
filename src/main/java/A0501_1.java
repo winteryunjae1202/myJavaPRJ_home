2980f1732c1ca31d444e0adc6cbc673d0f24c9ae
JoYunJae <winteryunjae1202@gmail.com>
2023-05-02 15:18:19 +0900

    별찍기

--- src/main/java/A0501_1.java
+++ src/main/java/A0501_1.java
@@ -1,6 +1,6 @@
 public class A0501_1 {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 6; i++){
             System.out.printf("*");
         }
     }
